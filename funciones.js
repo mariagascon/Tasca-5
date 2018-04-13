46b103038428a93ff4f0a818361c9f1010814059
Your Name <you@example.com>
2018-04-13 14:39:19 +0200

    * Inicio proyecto

---
+++ funciones.js
@@ -0,0 +1,8 @@
+var app = angular.module("app",[]);
+app.controller("ctrlMonedas", function($scope) {
+    $scope.convertir() = new function() {
+        alert($scope.valor);
+        alert($scope.moneda);
+    }
+});
+
